d27654bea749143cc1594b1f01d9b1588af47ef6
Phoenix Keeling <phoekeel196@outlook.com>
2020-12-08 14:42:35 -0500

    Add project files.

---
+++ AnythingCanHappen/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AnythingCanHappen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PicButton = new System.Windows.Forms.Button();
+            this.OutpouLabel1 = new System.Windows.Forms.Label();
+            this.DoYouLabel1 = new System.Windows.Forms.Label();
+            this.DoYouLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(775, 647);
+            this.label1.TabIndex = 0;
+            // 
+            // PicButton
+            // 
+            this.PicButton.BackColor = System.Drawing.Color.Black;
+            this.PicButton.Location = new System.Drawing.Point(38, 150);
+            this.PicButton.Name = "PicButton";
+            this.PicButton.Size = new System.Drawing.Size(725, 368);
+            this.PicButton.TabIndex = 1;
+            this.PicButton.UseVisualStyleBackColor = false;
+            // 
+            // OutpouLabel1
+            // 
+            this.OutpouLabel1.BackColor = System.Drawing.Color.Black;
+            this.OutpouLabel1.ForeColor = System.Drawing.Color.White;
+            this.OutpouLabel1.Location = new System.Drawing.Point(35, 26);
+            this.OutpouLabel1.Name = "OutpouLabel1";
+            this.OutpouLabel1.Size = new System.Drawing.Size(728, 121);
+            this.OutpouLabel1.TabIndex = 2;
+            // 
+            // DoYouLabel1
+            // 
+            this.DoYouLabel1.BackColor = System.Drawing.Color.Black;
+            this.DoYouLabel1.ForeColor = System.Drawing.Color.White;
+            this.DoYouLabel1.Location = new System.Drawing.Point(38, 525);
+            this.DoYouLabel1.Name = "DoYouLabel1";
+            this.DoYouLabel1.Size = new System.Drawing.Size(725, 46);
+            this.DoYouLabel1.TabIndex = 3;
+            // 
+            // DoYouLabel2
+            // 
+            this.DoYouLabel2.BackColor = System.Drawing.Color.Black;
+            this.DoYouLabel2.ForeColor = System.Drawing.Color.White;
+            this.DoYouLabel2.Location = new System.Drawing.Point(38, 575);
+            this.DoYouLabel2.Name = "DoYouLabel2";
+            this.DoYouLabel2.Size = new System.Drawing.Size(725, 52);
+            this.DoYouLabel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 669);
+            this.Controls.Add(this.DoYouLabel2);
+            this.Controls.Add(this.DoYouLabel1);
+            this.Controls.Add(this.OutpouLabel1);
+            this.Controls.Add(this.PicButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PicButton;
+        private System.Windows.Forms.Label OutpouLabel1;
+        private System.Windows.Forms.Label DoYouLabel1;
+        private System.Windows.Forms.Label DoYouLabel2;
+    }
+}
+
---
+++ AnythingCanHappen/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnythingCanHappen
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+
+            {
+                if (page == 1)
+                {
+                    page = 2;
+                }
+                else if (page == 2)
+                {
+                    page = 23;
+                }
+                else if (page == 23)
+                {
+                    page = 24;
+                }
+                else if (page == 20)
+                {
+                    page = 22;
+                }
+                else if (page == 18)
+                {
+                    page = 20;
+                }
+                else if (page == 10)
+                {
+                    page = 11;
+                }
+                else if (page == 12)
+                {
+                    page = 15;
+                }
+                else if (page == 15)
+                {
+                    page = 17;
+                }
+                else if (page == 13)
+                {
+                    page = 11;
+                }
+                else if (page == 3)
+                {
+                    page = 4;
+                }
+                else if (page == 4)
+                {
+                    page = 5;
+                }
+                else if (page == 7)
+                {
+                    page = 9;
+                }
+
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                if (page == 1)
+                {
+                    page = 3;
+                }
+                else if (page == 3)
+                {
+                    page = 7;
+                }
+                else if (page == 7)
+                {
+                    page = 8;
+                }
+                else if (page == 4)
+                {
+                    page = 6;
+                }
+                else if (page == 10)
+                {
+                    page = 12;
+                }
+                else if (page == 12)
+                {
+                    page = 13;
+                }
+                else if (page == 13)
+                {
+                    page = 3;
+                }
+                else if (page == 15)
+                {
+                    page = 16;
+                }
+                else if (page == 2)
+                {
+                    page = 18;
+                }
+                else if (page == 18)
+                {
+                    page = 19;
+                }
+                else if (page == 20)
+                {
+                    page = 21;
+                }
+                else if (page == 23)
+                {
+                    page = 25;
+                }
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                if (page == 1)
+                {
+                    page = 10;
+                }
+
+            }
+            switch (page)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+                case 12:
+
+                    break;
+                case 13:
+
+                    break;
+                case 15:
+
+                    break;
+                case 16:
+
+                    break;
+                case 17:
+
+                    break;
+                case 18:
+
+                    break;
+                case 19:
+
+                    break;
+                case 20:
+
+                    break;
+                case 21:
+
+                    break;
+                case 22:
+
+                    break;
+                case 23:
+
+                    break;
+                case 24:
+
+                    break;
+                case 25:
+
+                    break;
+            }
+        }
+    }
+}
